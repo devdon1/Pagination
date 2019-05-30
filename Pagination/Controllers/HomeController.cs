468da4cf1befa9e9ff4d9a73df056339183dd463
Dev Don <devdon1@outlook.com>
2019-05-30 13:58:01 +0200

    Change: Display the number of products

--- Pagination/Controllers/HomeController.cs
+++ Pagination/Controllers/HomeController.cs
@@ -12,7 +12,7 @@ namespace Pagination.Controllers
     {
         List<Product> products = new List<Product>();
 
-        decimal pageSize = 4;
+        decimal pageSize = 4; // maximum of products displayed per page
         decimal page = 1;
         decimal skipSize = 0;
         decimal numberOfPages = 0;
@@ -24,10 +24,33 @@ namespace Pagination.Controllers
 
         public IActionResult Products(decimal? pageNumber)
         {
+
             page = pageNumber == null ? 1 : (decimal) pageNumber;
 
+            // Populate some products for the store
             PopulateProducts();
 
+            // Get the number of products
+            int numberOfProducts = products.Count();
+
+            // Decide which text is on the site related to the number of products
+            switch (numberOfProducts)
+            {
+                case 0:
+                    ViewBag.NumberOfProducts = "Oh nooo! There are no in store :(";
+                  break;
+                case 1:
+                    ViewBag.NumberOfProducts = "There is only 1 product in store";
+                    break;
+                default:
+                    if (numberOfProducts > 1)
+                    {
+                        ViewBag.NumberOfProducts = "There are " + products.Count() + " products in store";
+                    }
+                    break;
+            }
+
+            // Set the number of pages for pagination
             numberOfPages = Math.Ceiling(products.Count() / pageSize);
             ViewBag.NumberOfPages = numberOfPages;
 
@@ -36,6 +59,9 @@ namespace Pagination.Controllers
             return View(products.Skip((int) skipSize).Take((int) pageSize));
         }
 
+        /// <summary>
+        /// Populate some fictional products for the store
+        /// </summary>
         private void PopulateProducts()
         {
             for (int i = 1; i <= 10; i++)
