f1c667c541a712cf2464017f9c59c887eda04c1a
Dev Don <devdon1@outlook.com>
2019-06-09 21:35:22 +0200

    WiP Products per page

--- Pagination/Controllers/ProductController.cs
+++ Pagination/Controllers/ProductController.cs
@@ -11,21 +11,13 @@ namespace Pagination.Controllers
     {
         List<Product> products = new List<Product>();
 
-        decimal pageSize = 5; // maximum of products displayed per page
-        decimal page = 1;
+        decimal pageSize = 0; // maximum of products displayed per page
+        decimal page = 0;
         decimal skipSize = 0;
         decimal numberOfPages = 0;
 
         public IActionResult Index(decimal? pageNumber, int productsPerPage)
         {
-            page = pageNumber == null ? 1 : (decimal)pageNumber;
-
-            // Set the pageSize based on the selected products per page
-            // After initial load of the page, the pageSize is set at 5
-            pageSize = productsPerPage == 0 ? 5 : productsPerPage;
-
-            ViewBag.producstsPersPage = pageSize;
-
             // Populate some products for the store
             PopulateProducts();
 
@@ -34,6 +26,14 @@ namespace Pagination.Controllers
 
             // Store the number of products in a Viewbag
             ViewBag.ProductCount = numberOfProducts;
+
+            page = pageNumber == null ? 1 : (decimal)pageNumber;
+
+            // Set the pageSize based on the selected products per page
+            // After initial load of the page, the pageSize is set at 5
+            pageSize = productsPerPage == 0 ? 5 : productsPerPage;
+
+            ViewBag.productsPersPage = pageSize;
 
             // Decide which text is displayed on the page related to the number of products (how many in stock)
             switch (numberOfProducts)
@@ -54,7 +54,7 @@ namespace Pagination.Controllers
 
             // Set the number of pages for pagination based on number of products and pageSize
             // And store the number of pages in a Viewbag
-            numberOfPages = Math.Ceiling(products.Count() / pageSize);
+            numberOfPages = Math.Ceiling(numberOfProducts / pageSize);
             ViewBag.NumberOfPages = numberOfPages;
 
             // Determine the skipSize (how many items in the list should be skipped)
@@ -78,10 +78,14 @@ namespace Pagination.Controllers
             }
         }
 
+        /// <summary>
+        /// Get the value of the selectlist: how many products should be displayed per page
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
         [HttpPost]
         public IActionResult NumberOfProducts(string value)
         {
-            //Products(null, Int32.Parse(value));
             return RedirectToAction("Index", new { productsPerPage = value });
         }
     }
